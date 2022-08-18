e8f65aa3a5140e30c36c7a9457195de993c8c2da
Elias Ellison <elias.ellison@gmail.com>
2022-08-18 20:16:09 +0000

    tmp

--- benchmarks/microbenchmarks/operatorbench.py
+++ benchmarks/microbenchmarks/operatorbench.py
@@ -16,9 +16,9 @@ aten = torch.ops.aten
 
 def compute_speedups(repeats, models, example_inputs):
     expected = models[0](*example_inputs)
-    for model in models[1:]:
-        actual = model(*example_inputs)
-        assert same(actual, expected), expected[0] - actual[0]
+    # for model in models[1:]:
+    #     actual = model(*example_inputs)
+        # assert same(actual, expected), expected[0] - actual[0]
 
     timings = np.zeros((repeats, len(models)), np.float64)
     for rep in range(repeats):
@@ -31,24 +31,58 @@ def compute_speedups(repeats, models, example_inputs):
     return np.median(timings, axis=0)
 
 
+def strip_overloads(gm):
+    """
+    Modifies the target of graph nodes in :attr:`gm` to strip overloads.
+    Args:
+        gm(fx.GraphModule): The input Fx graph module to be modified
+    """
+    for node in gm.graph.nodes:
+        if isinstance(node.target, torch._ops.OpOverload):
+            node.target = node.target.overloadpacket
+    gm.recompile()
+
+def convert_to_jit(gm, gm_args):
+    strip_overloads(gm)
+    try:
+        return torch.jit.script(gm)
+    except Exception :
+        pass
+    return torch.jit.trace(gm, gm_args)
+
 def microbenchmark(target, args, kwargs, dtype):
     gm, gm_args = gen_gm_and_inputs(target, args, kwargs)
-    compiled_fn = compile_fx(gm, gm_args)
-    cudagraphs_eager = cudagraphs_inner(gm, gm_args, copy_outputs=False)
+    torch.jit._builtins._register_builtin(torch.ops.aten.convolution_backward.default, "aten::convolution_backward")
+    # compiled_fn = compile_fx(gm, gm_args)
+    # cudagraphs_eager = cudagraphs_inner(gm, gm_args, copy_outputs=False)
+     
+    g = convert_to_jit(gm, gm_args)
     cudagraphs_jit = cudagraphs_inner(
-        torch.jit.trace(gm, gm_args), gm_args, copy_outputs=False
+        g, gm_args, copy_outputs=False
     )
 
-    repeats = 3
-    medians = compute_speedups(
-        repeats,
-        [cudagraphs_eager, cudagraphs_jit, compiled_fn],
-        gm_args,
-    )
+    # try:
+    #     g = torch.jit.trace(gm, gm_args)
+    # except Exception:
+    #     try:
+    #         g = torch.jit.script(gm.code)
+    #     except Exception
+    #         g = None
 
-    print(f"Perf for {target} {dtype} w/cudagraphs")
-    print(f"JIT NVFuser speedup over aten {medians[0]/medians[1]}")
-    print(f"Inductor speedup over aten {medians[1]/medians[2]}")
+    # except Exception as e:
+    #     import pdb; pdb.set_trace()
+    #     print(e)
+
+    # repeats = 3
+    # medians = compute_speedups(
+    #     repeats,
+    #     [cudagraphs_eager, cudagraphs_jit, compiled_fn],
+    #     gm_args,
+    # )
+
+    # print(f"Perf for {target} {dtype} w/cudagraphs")
+    # print(f"JIT NVFuser speedup over aten {medians[0]/medians[1]}")
+    # print(f"Inductor speedup over aten {medians[1]/medians[2]}")
 
 
 @click.command()
@@ -71,15 +105,20 @@ def benchmark(suite, op, dtype):
     assert dtype in ("float16", "float32"), f"got {dtype}"
     dtype = torch.float16 if dtype == "float16" else torch.float32
 
-    operator = eval(op)
-
-    # TODO - add more testing options / sweep over multiple inputs
-    # sweep for underperforming kernels
-
-    inp_gen = loader.get_inputs_for_operator(operator, dtype=torch.float)
-    for _ in range(1):
-        args, kwargs = next(inp_gen)
-        microbenchmark(operator, args, kwargs, dtype)
+    for operator in loader.get_all_ops():
+        if "convolution_backward" not in repr(operator):
+            continue
+        inp_gen = loader.get_inputs_for_operator(operator, dtype=dtype)
+        while True:
+            try:
+                inps = next(inp_gen)
+            except Exception as e:
+                print(operator)
+                print(e)
+                break
+            args, kwargs = next(inp_gen)
+            microbenchmark(operator, args, kwargs, dtype)
+            break
 
 
 if __name__ == "__main__":
